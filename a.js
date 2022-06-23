a39d1025cb4812758c35e4a97d601fe157867017
grahagandangr <grahagandangr@gmail.com>
2022-06-24 05:54:13 +0700

    remove test file

--- a.js
+++
@@ -1,7 +0,0 @@
-const bcryptjs = require('bcryptjs')
-const salt = bcryptjs.genSaltSync(10)
-const hash = bcryptjs.hashSync('admin', salt)
-console.log(hash);
-
-console.log(bcryptjs.compareSync('admin', hash))
-console.log(bcryptjs.compareSync('nomos', hash))
